c1ff75a99c8c23adac9f59f8bfada25ef19f4735
Syed-Urwah <syedurwahdev@gmail.com>
2024-10-15 18:33:47 +0500

    test

--- src/app/api/puppeteer/route.js
+++ src/app/api/puppeteer/route.js
@@ -1,35 +1,28 @@
-import { NextResponse } from "next/server";
-import puppeteer from "puppeteer";
+const puppeteer = require("puppeteer-core");
+const chromium = require("@sparticuz/chromium");
 
 export async function POST(){
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    
-    // Navigate the page to a URL.
-    await page.goto('https://developer.chrome.com/');
-    
-    // Set screen size.
-    await page.setViewport({width: 1080, height: 1024});
-    
-    // Type into search box.
-    await page.locator('.devsite-search-field').fill('automate beyond recorder');
-    
-    // Wait and click on first result.
-    await page.locator('.devsite-result-item-link').click();
-    
-    // Locate the full title with a unique string.
-    const textSelector = await page
-      .locator('text/Customize and automate')
-      .waitHandle();
-    const fullTitle = await textSelector?.evaluate(el => el.textContent);
-    
-    // Print the full title.
-    console.log('The title of this blog post is "%s".', fullTitle);
-    
-    await browser.close();
+  const browser = await puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath(),
+    headless: chromium.headless,
+    ignoreHTTPSErrors: true,
+  });
+
+  const page = await browser.newPage();
+
+  await page.goto("https://www.example.com", { waitUntil: "networkidle0" });
+
+  console.log("Chromium:", await browser.version());
+  console.log("Page Title:", await page.title());
+
+  await page.close();
+
+  await browser.close();
 
     return new Response(
-        JSON.stringify({ fullTitle }),
+        JSON.stringify({ fullTitle: "success" }),
         { headers: { "Content-Type": "application/json" } }
       );
 }
